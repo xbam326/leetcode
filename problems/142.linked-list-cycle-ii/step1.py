117ca2e9764a7502544e442a0e88c4b52267a671
Masakuni <xxbambooxx@icloud.com>
2025-12-25 02:47:35 +0900

    step1 floyd

--- problems/142.linked-list-cycle-ii/step1.py
+++ problems/142.linked-list-cycle-ii/step1.py
@@ -13,15 +13,37 @@
 
 
 class Solution:
+    # def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     reached = set()
+    #     current = head
+    #     while current is not None:
+    #         if current in reached:
+    #             return current
+    #         reached.add(current)
+    #         current = current.next
+    #     return None
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        reached = set()
-        current = head
-        while current is not None:
-            if current in reached:
-                return current
-            reached.add(current)
-            current = current.next
-        return None
+        # step1 Floydの循環検出でloopするかどうかを判定する
+        slow = fast = head
+        hasLoop = False
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                hasLoop = True
+                break
+        if hasLoop != True:
+            return None
+
+        # step2
+        # 片方のNodeをheadに設定し、headと循環検出で合流した点からそれぞれ1stepずつたどる
+        # 合流した点がloopの開始の点になる
+        nodeFromHead = head
+        nodeFromMeetingPoint = slow
+        while nodeFromHead != nodeFromMeetingPoint:
+            nodeFromHead = nodeFromHead.next
+            nodeFromMeetingPoint = nodeFromMeetingPoint.next
+        return nodeFromHead
 
 
 # @lc code=end
