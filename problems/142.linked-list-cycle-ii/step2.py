d32b3ee23c5e7e899a9a7611d97740efc1d517e5
Masakuni <xxbambooxx@icloud.com>
2025-12-24 19:39:56 +0900

    wip:step2

---
+++ problems/142.linked-list-cycle-ii/step2.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=142 lang=python3
+#
+# [142] Linked List Cycle II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        reached = set()
+        current = head
+        if current:
+            if current is reached:
+                return current
+            reached.add(current)
+            current = current.next
+        return None
+
+
+# @lc code=end
